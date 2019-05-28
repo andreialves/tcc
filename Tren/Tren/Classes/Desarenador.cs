39a40694d301ccc9f236786eff1c0ca1e029309c
Andrei Alves <andreialvesccp@gmail.com>
2019-05-28 00:14:27 -0300

    Terminar a classe desarenador e adicionar um framework de criação de telas

--- Tren/Tren/Classes/Desarenador.cs
+++ Tren/Tren/Classes/Desarenador.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace Tren.Classes
 {
     class Desarenador : UnidadePreliminar
     {
+        IFormatProvider format = CultureInfo.InvariantCulture;
         private double velocidadeEfluente;
         private double velocidadeTeste;
         private double larguraDesarenador;
@@ -20,6 +22,33 @@ namespace Tren.Classes
         private double alturaGeradaAreiaDia;
 
 
+        // Calculo da altura provisório
+        private double alturaMin;
+        private double alturaMax;
+      
+
+        // Construtores do Desarenador
+
+        public Desarenador(double velocidadeEfluente, double alturaMax, double alturaMin)
+        {
+            this.velocidadeEfluente = velocidadeEfluente;
+            calculaLarguraDesarenador(VazaoMaxFut, alturaMax, this.velocidadeEfluente);
+            calculaAreaSecao(alturaMin);
+
+            corrigeVelocidade(velocidadeEfluente);
+
+
+            calculaComprimento(alturaMax);
+            calculaAreaSuperficial();
+            calculaTaxaAplicacaoSuperficial();
+            calculaFrequenciaLimpeza(0.9);
+
+
+
+            this.alturaMin = alturaMin;
+            this.alturaMax = alturaMax;
+        }
+
         public Desarenador( double velocidadeEfluente, double alturaMax, double alturaMin,
             double vazaoMax, double vazaoMed, double vazaoMin, double vazaoMaxFut, double vazaoMedFut, double vazaoMinFut)
             : base(vazaoMax, vazaoMed, vazaoMin, vazaoMaxFut, vazaoMedFut, vazaoMinFut){
@@ -27,11 +56,19 @@ namespace Tren.Classes
             this.velocidadeEfluente = velocidadeEfluente;
             calculaLarguraDesarenador(VazaoMaxFut, alturaMax, this.velocidadeEfluente);
             calculaAreaSecao(alturaMin);
-            verificaVelocidade();
+            
+            corrigeVelocidade(velocidadeEfluente);
+
+           
             calculaComprimento(alturaMax);
             calculaAreaSuperficial();
             calculaTaxaAplicacaoSuperficial();
             calculaFrequenciaLimpeza(0.9);
+
+
+
+            this.alturaMin = alturaMin;
+            this.alturaMax = alturaMax;
         }
 
         public Desarenador(double velocidadeEfluente, double alturaMax, double alturaMin,
@@ -41,36 +78,79 @@ namespace Tren.Classes
             this.velocidadeEfluente = velocidadeEfluente;
             calculaLarguraDesarenador(VazaoMaxFut, alturaMax, this.velocidadeEfluente);
             calculaAreaSecao(alturaMin);
-            verificaVelocidade();
+            corrigeVelocidade(velocidadeEfluente);
             calculaComprimento(alturaMax);
             calculaAreaSuperficial();
             calculaTaxaAplicacaoSuperficial();
             calculaFrequenciaLimpeza(0.9);
+
+
+
+            this.alturaMin = alturaMin;
+            this.alturaMax = alturaMax;
         }
 
+        //
+
+
+        // Calcula a largura inicial do desarenador
         public void calculaLarguraDesarenador(double vazaoMaxFut, double alturaMax, double velocidadeEfluente)
         {
             larguraDesarenador = (VazaoMaxFut / 1000) / (alturaMax * velocidadeEfluente);
         }
 
+        // Calcula a area da seção do canal inicial
         public void calculaAreaSecao(double alturaMin) 
         {
             areaSecaoDesarenador = alturaMin * larguraDesarenador;
         }
 
-        public void verificaVelocidade(){
+        // Corrige a velocidade do efluente
+        public void corrigeVelocidade(double vh){
             try
             {
-                velocidadeTeste = (VazaoMinFut / 1000) / areaSecaoDesarenador;
-                while(velocidadeTeste > 4){
-                    Console.WriteLine("Calcule uma nova Largura para o Desarenador");
-                    // fazer o calculo velocidadeTeste = (VazaoMin / 1000) / areaSecaoDesarenador;
-                    velocidadeTeste = 3;
+                if (vh < 0.2 || vh > 0.4)
+                {
+                    Console.WriteLine("Valor fora do intervalo (0.2 , 0.4)");
+                    Console.WriteLine(vh);
+                    vh = double.Parse(Console.ReadLine(), format);
+                }
+                if (CalculaVelocidadeTeste(0.2) && CalculaVelocidadeTeste(0.4))
+                {
+                   
+                        throw new Exception("Largura Fora das Normas, impossível realizar a operação!");
                 }
 
-            }catch(Exception)
+                if (CalculaVelocidadeTeste(vh))
+                {
+                    Console.WriteLine("Sua velocidade está fora das normas, por favor insira uma nova velocidade: ");
+                    vh = double.Parse(Console.ReadLine(), format);
+                    while(CalculaVelocidadeTeste(vh))
+                    {
+                        Console.WriteLine(vh);
+                        if (vh < 0.2 || vh > 0.4)
+                        {
+                            Console.WriteLine("Valor fora do intervalo (0.2 , 0.4)");
+                            vh = double.Parse(Console.ReadLine(), format);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sua velocidade está fora das normas, por favor insira uma nova velocidade: ");
+                            vh = double.Parse(Console.ReadLine(), format);
+                        }
+
+                    }
+
+                    velocidadeEfluente = vh;
+                    larguraDesarenador = VazaoMaxFut / 1000 / (alturaMax * velocidadeEfluente);
+                    areaSecaoDesarenador = alturaMin * larguraDesarenador;
+                    
+                }
+                velocidadeTeste = (VazaoMinFut / 1000) / areaSecaoDesarenador;
+
+            }catch(Exception e)
             {
-                Console.WriteLine("ERRO - Área da Seção do Canal não foi Calculado");
+                Console.WriteLine(e.Message);
             }
         }
 
@@ -87,15 +167,43 @@ namespace Tren.Classes
             }
             catch (Exception e)
             {
-                throw e.InnerException; 
+                Console.WriteLine(e.Message); 
             }
         }
-
+ 
+        /* Calcula a taxa de aplicação superficial, e verifica se ela está dentro dos padões. Caso não esteja pede pra corrigir 
+         * velocidade e recalcula todos os coeficientes
+        */
         public void calculaTaxaAplicacaoSuperficial()
         {
             taxaAplicacaoSueprficial = (VazaoMaxFut / 1000) * areaSuperficial;
+            try
+            {
+                if (verificaExtremos(0.2) && verificaExtremos(0.4))
+                {
+                    throw new Exception("Fora do intervalo, impossível fazer a verificação");
+                }
 
-            // Falta fazer a verificação do VH
+                if (taxaAplicacaoSueprficial > 1300 || taxaAplicacaoSueprficial < 600)
+                {
+                    do
+                    {
+                        Console.WriteLine("TAS " + taxaAplicacaoSueprficial + "fora da faixa, por favor informe uma nova velocidade: ");
+                        double vh;
+                        vh = double.Parse(Console.ReadLine(), format);
+                        Console.WriteLine(vh);
+                        corrigeVelocidade(vh);
+                        calculaAreaSuperficial();
+                        taxaAplicacaoSueprficial = (VazaoMaxFut / 1000) * areaSuperficial;
+
+
+                    } while (taxaAplicacaoSueprficial > 1300 || taxaAplicacaoSueprficial < 600);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
 
         public void calculaFrequenciaLimpeza(double taxaDiariaAreia)
@@ -103,21 +211,6 @@ namespace Tren.Classes
             volDiarioAreiaRetido = (taxaDiariaAreia * 1000) * (VazaoMedFut/1000) * 86400;
             alturaGeradaAreiaDia = (volDiarioAreiaRetido / 1000) * areaSuperficial;
             frequenciaLimpeza = (int)Math.Round((0.2 * alturaGeradaAreiaDia));
-        }
-
-
-
-
-        public double VelocidadeEfluente
-        {
-            get
-            {
-                return velocidadeEfluente;
-            }
-            set
-            {
-                velocidadeEfluente = value;
-            }
         }
 
         public override void imprime()
@@ -128,11 +221,36 @@ namespace Tren.Classes
             Console.WriteLine(areaSuperficial);
             Console.WriteLine("Altura m");
             Console.WriteLine("Altura M");
+            Console.WriteLine(velocidadeTeste);
             Console.WriteLine(taxaAplicacaoSueprficial);
             Console.WriteLine(alturaGeradaAreiaDia);
             Console.WriteLine(frequenciaLimpeza);
         }
 
-        
+        /**
+         * 
+         * Função para verificar se a velocidade informada é menor que o recomendado pela norma
+         * 
+        **/
+        private bool CalculaVelocidadeTeste(double velocidade)
+        {
+            double largura, area, vh;
+            largura = (VazaoMaxFut / 1000) / (alturaMax * velocidade);
+            area = alturaMin * largura;
+            vh = VazaoMinFut / 1000 / area;
+            
+            return vh > 4;
+        }
+
+        private bool verificaExtremos(double velocidade)
+        {
+            double largura, area, vh, superficial, taxa;
+            largura = (VazaoMaxFut / 1000) / (alturaMax * velocidade);
+            area = alturaMin * largura;
+            vh = VazaoMinFut / 1000 / area;
+            superficial = comprimentoDesarenador * largura;
+            taxa = (VazaoMaxFut / 1000) * superficial;
+            return (taxa > 1300 || taxa < 600);
+        }
     }
 }
--- Tren/Tren/Program.cs
+++ Tren/Tren/Program.cs
@@ -11,18 +11,23 @@ namespace Tren
     {
         static void Main(string[] args)
         {
-			UnidadePreliminar a = new UnidadePreliminar(40, 30, 19, 119, 60, 50);
+			UnidadePreliminar a = new UnidadePreliminar(60000, 70411, 150, 120, 140.822, 0, 0.2);
 			// a.imprime();
-			// Desarenador b = new Desarenador(0.2, 5, 3, 60000, 70411, 150, 120, 140, 0, 0.2);
-			CalhaParshall cp = new CalhaParshall();
-			Console.WriteLine("dados: " + cp.getW + " " + cp.getN + " " + cp.getK + " " + cp.getHMin + " " + cp.getHMax + " " + cp.gethMin + " " + cp.gethMax);
-            //SequenciaPreliminar sequencia = new SequenciaPreliminar(b);
-			//sequencia.adicionar(a);
 			
-            /*foreach (UnidadePreliminar i in sequencia.getUnidadePreliminar())
+			CalhaParshall cp = new CalhaParshall();
+            Desarenador b = new Desarenador(0.3, cp.getHMax, cp.getHMin);
+            //Console.WriteLine("dados: " + cp.getW + " " + cp.getN + " " + cp.getK + " " + cp.getHMin + " " + cp.getHMax + " " + cp.gethMin + " " + cp.gethMax);
+            SequenciaPreliminar sequencia = new SequenciaPreliminar(cp);
+            sequencia.adicionar(b);
+            //b.imprime();
+
+            foreach (UnidadePreliminar i in sequencia.getUnidadePreliminar())
             {
                 i.imprime();
-            }*/
+            }
+
+          
+
             Console.ReadKey();
         }
     }
