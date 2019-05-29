ba0dcb1887a8cb1e050b726c34bc11584a59d4ed
Andrei Alves <andreialvesccp@gmail.com>
2019-05-28 23:26:15 -0300

    Criação da Grade

---
+++ Tren/Tren/Classes/Grade.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tren.Classes
+{
+    class Grade : UnidadePreliminar
+    {
+        private int tipoGrade;
+        private double espessuraGrade;
+        private double espacamentoGrade;
+        private double eficienciaGrade;
+        private double areaUtilMax;
+        private double areaUtilMin;
+        private double secaoCanalMax;
+        private double larguraGradeMax;
+        private double secaoCanalMin;
+        private double larguraGradeMin;
+        private double velocidadeMin;
+        private double velocidadeAproxMax;
+        private double velocidadeAproxMin;
+        private double perdaCargaMax;
+        private double perdaCargaMin;
+        private double perdaCargaMaxObs;
+        private double perdaCargaMinObs;
+        private double perdaCargaTotal;
+        private int numeroBarras;
+        private int numeroEspacamentos;
+        private int correcaoBarraLateral;
+        private double difRetLateral;
+        private double comprimentoGrade;
+
+
+        public Grade(int espessura, int espacamento)
+        {
+            espessuraGrade = espessura;
+            espacamentoGrade = espacamento;
+        }
+
+    }
+}
