297cd936fea5db0b97ddcf361d15808a40cffd05
Filipe-MN <filipemn_uba@hotmail.com>
2019-06-14 15:17:57 -0300

    Design da janela

--- Tren/Tren/Program.cs
+++ Tren/Tren/Program.cs
@@ -11,24 +11,11 @@ namespace Tren {
     class Program {
 		[STAThread]
         static void Main(string[] args) {
-			/*try {
-				CentralTratamento ct = new CentralTratamento(1000, 600, 400, 2000, 1200, 400, 6000, 12000);
-				SequenciaPreliminar sp = new SequenciaPreliminar(ct);
-				ct.adicionar(sp);
-				CalhaParshall cp = new CalhaParshall(sp);
-				CaixaSAO cSao = new CaixaSAO(1.0, 0.8, 1.37, sp);
-			}catch(Exception e) {
-				Console.WriteLine(e.Message);
-			}*/
-
+			
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new InicioView());
-
-            //Console.WriteLine("Fim do programa!");
-
-				
-           // Console.ReadKey();
+			
         }
     }
 }
--- Tren/Tren/Views/InicioView.Designer.cs
+++ Tren/Tren/Views/InicioView.Designer.cs
@@ -23,6 +23,7 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InicioView));
 			this.mainPanel = new System.Windows.Forms.Panel();
 			this.SuspendLayout();
 			// 
@@ -34,16 +35,22 @@
 			this.mainPanel.Size = new System.Drawing.Size(800, 450);
 			this.mainPanel.TabIndex = 0;
 			// 
-			// Inicio
+			// InicioView
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BorderStyle = MetroFramework.Drawing.MetroBorderStyle.FixedSingle;
 			this.ClientSize = new System.Drawing.Size(840, 530);
 			this.Controls.Add(this.mainPanel);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
 			this.MinimumSize = new System.Drawing.Size(800, 350);
-			this.Name = "Inicio";
-			this.Style = MetroFramework.MetroColorStyle.Green;
+			this.Name = "InicioView";
+			this.Opacity = 0.997D;
+			this.Resizable = false;
+			this.Style = MetroFramework.MetroColorStyle.Teal;
 			this.Text = "TREN";
+			this.Theme = MetroFramework.MetroThemeStyle.Light;
 			this.ResumeLayout(false);
 
         }
